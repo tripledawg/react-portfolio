0088cfaa80288f9a6a4ce431f1d8eb805c9a3899
tripledawg <iamlesliecrouch>
2022-03-15 15:30:45 -0400

    .gitignore and boiler plate code added

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Here we are importing a CSS file as a dependency
+import '../styles.css'; //correct route? 
+
+function Header() {
+  return (
+    <header className="footer">
+      <h1>Home</h1>
+    </header>
+  );
+}
+
+export default Footer;
